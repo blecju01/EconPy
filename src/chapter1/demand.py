037b2eb9b02de8b8b24a1be56425e2233335c661
TylerMan <tbaughcome@gmail.com>
2021-12-29 21:19:14 -0500

    learning...

--- src/chapter1/demand.py
+++ src/chapter1/demand.py
@@ -1,8 +1,21 @@
-def demand():
+def demand(equation):
     # Parse equation
     """
      --expect a string of the form Q = P1 + P2 +
                  ... { other parameters } + constant
     """
+    
     # Determine demand curve relationship
     # Return graphic using matplotlib & numpy
+
+
+def supply(equation):
+    # Parse equation
+    # Determine supply curve relationship
+    # Return graphic using matplotlib & numpy
+    pass
+
+
+# demand("Q = 2P + 200")
+# demand('Q = 2P + 200')
+# demand("Q = 5P + 300")
