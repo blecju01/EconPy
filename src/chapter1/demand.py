14059d66db42e4348fa4163af4f6c470b2d37dcd
TylerMan <tbaughcome@gmail.com>
2021-12-29 21:47:11 -0500

    plotDemand start

--- src/chapter1/demand.py
+++ src/chapter1/demand.py
@@ -1,12 +1,18 @@
-def demand(equation):
-    # Parse equation
-    """
-     --expect a string of the form Q = P1 + P2 +
-                 ... { other parameters } + constant
-    """
-    
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotDemand(slope, intercept, x_range):
     # Determine demand curve relationship
     # Return graphic using matplotlib & numpy
+    x = np.linspace(x_range[0],x_range[1],100)
+    y = 2*x+1
+    plt.plot(x, y, '-r', label='y=2x+1')
+    plt.title('Graph of y=2x+1')
+    plt.xlabel('x', color='#1C2833')
+    plt.ylabel('y', color='#1C2833')
+    plt.legend(loc='upper left')
+    plt.grid()
+    plt.show()
 
 
 def supply(equation):
@@ -15,7 +21,8 @@ def supply(equation):
     # Return graphic using matplotlib & numpy
     pass
 
+# plotDemand(4.5, -1, [0,0])
+# plotDemand("Hello", -1, [0,0])
 
-# demand("Q = 2P + 200")
-# demand('Q = 2P + 200')
-# demand("Q = 5P + 300")
+
+
